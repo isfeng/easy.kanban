2b7f0e76f9938a57be931f8d3628a62940402659
Feng. H <sergio.huang@gmail.com>
2012-12-24 17:06:00 +0800

    wa..hahaha...

---
+++ public/javascripts/kanban.js
@@ -0,0 +1,78 @@
+//google web font
+WebFontConfig = {
+	google : {
+		families : [ 'Shadows+Into+Light::latin' ]
+	}
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http')
+			+ '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
+  
+var Sticky = new Class
+({
+	
+	Implements:[Options, Events],
+	
+	options:{
+		
+	},
+	
+	initialize:function()
+	{
+		
+	},
+	
+	tear: function()
+	{
+		
+	}
+	
+});
+
+var Postit = new Class
+({
+	
+	Implements:[Options, Events],
+	
+	options:{
+		
+	},
+	
+	initialize:function()
+	{
+		
+	},
+	
+	pull: function()
+	{
+		
+	}
+	
+});
+
+var Kanban = new Class
+({
+	
+	Implements:[Options, Events],
+	
+	options:{
+		
+	},
+	
+	initialize:function()
+	{
+		
+	},
+	
+	stick: function(sticky)
+	{
+		
+	}
+	
+});
