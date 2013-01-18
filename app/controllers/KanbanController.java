6981899c83ddf4dcce06ed55405e3bde36c2a4ab
feng <yespeak@gmail.com>
2013-01-18 13:57:58 +0800

    test google analytics

--- app/controllers/KanbanController.java
+++ app/controllers/KanbanController.java
@@ -27,8 +27,8 @@ public class KanbanController extends Controller
 		renderArgs.put("kanban", Kanban.findById(id));
 		render();
 	}
-
-
+	
+	
 	public static void create(@Required String name, @Required String goal, String values)
 	{
 		if (validation.hasErrors())
@@ -37,20 +37,20 @@ public class KanbanController extends Controller
 			validation.keep(); // keep the errors for the next request
 			_new();
 		}
-
+		
 		SocialUser socialUser = SecureSocial.getCurrentUser();
 		User _user = User.find("bySocialID", socialUser.id.id).first();
-
-
+		
+		
 		Kanban k = new Kanban();
 		k.name = name;
 		k.goal = goal;
 		k.board = Board.getDefaultBoard();
 		k.save();
-
+		
 		UserKanban uk = new UserKanban(_user, k);
 		uk.save();
-
+		
 		if (values != null && !values.equals(""))
 		{
 			String valuearr[] = values.split(",");
@@ -60,11 +60,11 @@ public class KanbanController extends Controller
 				value.save();
 			}
 		}
-
+		
 		index();
 	}
-
-
+	
+	
 	/**
 	 * cant update value stream currently
 	 * @param id
@@ -75,29 +75,29 @@ public class KanbanController extends Controller
 	{
 		SocialUser socialUser = SecureSocial.getCurrentUser();
 		User user = User.find("bySocialID", socialUser.id.id).first();
-
+		
 		Kanban k = Kanban.findById(id);
 		k.name = name;
 		k.goal = goal;
 		k.save();
-
+		
 		index();
 	}
-
-
+	
+	
 	public static void delete(long id)
 	{
 		Kanban k = Kanban.findById(id);
-
+		
 		TextNote.delete("kanban", k);
 		ValueStream.delete("kanban=?", k);
 		UserKanban.delete("kanban", k);
-
+		
 		k.delete();
 		index();
 	}
-
-
+	
+	
 	public static void index()
 	{
 		SocialUser socialUser = SecureSocial.getCurrentUser();
@@ -106,15 +106,20 @@ public class KanbanController extends Controller
 		renderArgs.put("kanbans", kanbans);
 		render();
 	}
-
-
+	
+	
 	public static void notes(long id)
 	{
 		List<TextNote> notes = TextNote.find("byKanban", Kanban.findById(id)).fetch();
+		for (TextNote textNote : notes)
+		{
+			textNote.kanban = null;
+			textNote.value = null;
+		}
 		renderJSON(notes);
 	}
-
-
+	
+	
 	public static void getBackground(long id)
 	{
 		Kanban k = Kanban.findById(id);
@@ -128,8 +133,8 @@ public class KanbanController extends Controller
 		m.put("stream", vs);
 		renderJSON(m);
 	}
-
-
+	
+	
 	public static void setBackground(long id, String dataURL)
 	{
 		Kanban k = Kanban.findById(id);
@@ -137,54 +142,54 @@ public class KanbanController extends Controller
 		k.save();
 		renderJSON(new String("OK"));
 	}
-
-
+	
+	
 	public static void _new()
 	{
 		render();
 	}
-
-
+	
+	
 	public static void _update(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-
-
+	
+	
 	public static void share(long id, String email)
 	{
 		User u = User.find("byEmail", email).first();
 		Kanban k = Kanban.findById(id);
-
+		
 		UserKanban uk = UserKanban.find("byUserAndKanban", u, k).first();
 		if (uk == null)
 		{
 			UserKanban share = new UserKanban(u, k);
 			share.save();
 		}
-
+		
 		index();
 	}
-
-
+	
+	
 	public static void _share(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-
-
+	
+	
 	@Before(unless = { "create", "index", "_new" })
 	static void checkKanbanAccessRight(long id)
 	{
 		SocialUser suser = SecureSocial.getCurrentUser();
 		if (suser == null)
 			SecureSocial.login();
-
+		
 		UserKanban uk = UserKanban.findBySocialIDAndKanbanID(suser.id.id, id);
 		if (uk == null)
 			forbidden();
 	}
-
+	
 }
