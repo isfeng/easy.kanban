db0e96cd453ac3318f9a7112af89cb418d95b044
feng <yespeak@gmail.com>
2013-01-24 10:08:10 +0800

    Revert "what ?"
    This reverts commit 646b539467406f8464db4405b7af3129e26b976d.

--- app/controllers/KanbanController.java
+++ app/controllers/KanbanController.java
@@ -27,9 +27,9 @@ public class KanbanController extends Controller
 		renderArgs.put("kanban", Kanban.findById(id));
 		render();
 	}
-
-
-	public static void create(@Required String name, String goal, @Required int workflow, String workflow_customize, @Required int size)
+	
+	
+	public static void create(@Required String name, @Required String goal, @Required int workflow, String workflow_customize, @Required int size)
 	{
 		if (validation.hasErrors())
 		{
@@ -37,11 +37,11 @@ public class KanbanController extends Controller
 			validation.keep(); // keep the errors for the next request
 			_new();
 		}
-
+		
 		SocialUser socialUser = SecureSocial.getCurrentUser();
 		User _user = User.find("bySocialID", socialUser.id.id).first();
-
-
+		
+		
 		Board b = null;
 		switch (size)
 		{
@@ -58,16 +58,16 @@ public class KanbanController extends Controller
 				error();
 				break;
 		}
-
+		
 		Kanban k = new Kanban();
 		k.name = name;
 		k.goal = goal;
 		k.board = b;
 		k.save();
-
+		
 		UserKanban uk = new UserKanban(_user, k);
 		uk.save();
-
+		
 		switch (workflow)
 		{
 			case 1:
@@ -86,11 +86,11 @@ public class KanbanController extends Controller
 				error();
 				break;
 		}
-
-		show(k.id);
+		
+		index();
 	}
-
-
+	
+	
 	private static void createWorkflow(String values, Kanban k)
 	{
 		if (values != null && !values.equals(""))
@@ -103,8 +103,8 @@ public class KanbanController extends Controller
 			}
 		}
 	}
-
-
+	
+	
 	/**
 	 * cant update value stream currently
 	 * @param id
@@ -115,29 +115,29 @@ public class KanbanController extends Controller
 	{
 		SocialUser socialUser = SecureSocial.getCurrentUser();
 		User user = User.find("bySocialID", socialUser.id.id).first();
-
+		
 		Kanban k = Kanban.findById(id);
 		k.name = name;
 		k.goal = goal;
 		k.save();
-
+		
 		index();
 	}
-
-
+	
+	
 	public static void delete(long id)
 	{
 		Kanban k = Kanban.findById(id);
-
+		
 		TextNote.delete("kanban", k);
 		ValueStream.delete("kanban=?", k);
 		UserKanban.delete("kanban", k);
-
+		
 		k.delete();
 		index();
 	}
-
-
+	
+	
 	public static void index()
 	{
 		SocialUser socialUser = SecureSocial.getCurrentUser();
@@ -146,8 +146,8 @@ public class KanbanController extends Controller
 		renderArgs.put("kanbans", kanbans);
 		render();
 	}
-
-
+	
+	
 	public static void notes(long id)
 	{
 		List<TextNote> notes = TextNote.find("byKanban", Kanban.findById(id)).fetch();
@@ -158,8 +158,8 @@ public class KanbanController extends Controller
 		}
 		renderJSON(notes);
 	}
-
-
+	
+	
 	public static void getBackground(long id)
 	{
 		Kanban k = Kanban.findById(id);
@@ -173,8 +173,8 @@ public class KanbanController extends Controller
 		m.put("stream", vs);
 		renderJSON(m);
 	}
-
-
+	
+	
 	public static void setBackground(long id, String dataURL)
 	{
 		Kanban k = Kanban.findById(id);
@@ -182,54 +182,54 @@ public class KanbanController extends Controller
 		k.save();
 		renderJSON(new String("OK"));
 	}
-
-
+	
+	
 	public static void _new()
 	{
 		render();
 	}
-
-
+	
+	
 	public static void _update(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-
-
+	
+	
 	public static void share(long id, String email)
 	{
 		User u = User.find("byEmail", email).first();
 		Kanban k = Kanban.findById(id);
-
+		
 		UserKanban uk = UserKanban.find("byUserAndKanban", u, k).first();
 		if (uk == null)
 		{
 			UserKanban share = new UserKanban(u, k);
 			share.save();
 		}
-
+		
 		index();
 	}
-
-
+	
+	
 	public static void _share(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-
-
+	
+	
 	@Before(unless = { "create", "index", "_new" })
 	static void checkKanbanAccessRight(long id)
 	{
 		SocialUser suser = SecureSocial.getCurrentUser();
 		if (suser == null)
 			SecureSocial.login();
-
+		
 		UserKanban uk = UserKanban.findBySocialIDAndKanbanID(suser.id.id, id);
 		if (uk == null)
 			forbidden();
 	}
-
+	
 }
