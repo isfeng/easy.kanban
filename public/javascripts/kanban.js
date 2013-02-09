fde80b66c9f1a819f35e059a32de20d94bb9553c
sergio.huang <sergio.huang@gmail.com>
2013-02-09 16:38:08 +0800

    fix move icon

--- public/javascripts/kanban.js
+++ public/javascripts/kanban.js
@@ -561,12 +561,12 @@ var Kanban = new Class({
 	{
 		$(mover).addEvent("click", function(){
 			if(this.current_action != 'drag')
-			{
-				this.current_action = 'drag';
+			{		
 				this.dragScroller.attach();
-				$('kanban').setStyle('cursor', 'move');
-				this.activateTool(mover);
-			}			
+			}
+			this.current_action = 'drag';				
+			$('kanban').setStyle('cursor', 'move');
+			this.activateTool(mover);	
 		}.bind(this));
 	},
 
