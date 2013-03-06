d29f55dd7b46384942e1a71012eccf3081b84241
sergio.huang <sergio.huang@gmail.com>
2013-03-07 00:08:35 +0800

    add AdminController

--- app/controllers/KanbanController.java
+++ app/controllers/KanbanController.java
@@ -36,8 +36,8 @@ public class KanbanController extends Controller
 		renderArgs.put("debug", Play.configuration.getProperty("pusher.debug"));
 		render();
 	}
-	
-	
+
+
 	public static void create(@Required String name, String goal, @Required int workflow, String workflow_customize, @Required String size, boolean _public)
 	{
 		if (validation.hasErrors())
@@ -46,13 +46,13 @@ public class KanbanController extends Controller
 			validation.keep(); // keep the errors for the next request
 			_new();
 		}
-		
+
 		SocialUser socialUser = SecureSocial.getCurrentUser();
 		User _user = User.find("bySocialID", socialUser.id.id).first();
-		
-		
+
+
 		Board b = Board.getDefaultBoard(size);
-		
+
 		Kanban k = new Kanban();
 		k.name = name;
 		// k.goal = goal;
@@ -60,10 +60,10 @@ public class KanbanController extends Controller
 		if (_public)
 			k._public = true;
 		k.save();
-		
+
 		UserKanban uk = new UserKanban(_user, k);
 		uk.save();
-		
+
 		createWorkflow(workflow_customize, k);
 		// switch (workflow)
 		// {
@@ -83,11 +83,11 @@ public class KanbanController extends Controller
 		// error();
 		// break;
 		// }
-		
+
 		show(k.id, true);
 	}
-	
-	
+
+
 	private static void createWorkflow(String values, Kanban k)
 	{
 		if (values != null && !values.equals(""))
@@ -100,8 +100,8 @@ public class KanbanController extends Controller
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * cant update value stream currently
 	 * @param id
@@ -115,21 +115,21 @@ public class KanbanController extends Controller
 		k.save();
 		index();
 	}
-	
-	
+
+
 	public static void delete(long id)
 	{
 		Kanban k = Kanban.findById(id);
-		
+
 		TextNote.delete("kanban", k);
 		ValueStream.delete("kanban=?", k);
 		UserKanban.delete("kanban", k);
-		
+
 		k.delete();
 		index();
 	}
-	
-	
+
+
 	public static void index()
 	{
 		SocialUser socialUser = SecureSocial.getCurrentUser();
@@ -138,24 +138,24 @@ public class KanbanController extends Controller
 		renderArgs.put("kanbans", kanbans);
 		render();
 	}
-	
-	
+
+
 	public static void notes(long id)
 	{
 		List<StickyNote> tnotes = TextNote.find("byKanban", Kanban.findById(id)).fetch();
 		List<StickyNote> dnotes = DrawNote.find("byKanban", Kanban.findById(id)).fetch();
 		tnotes.addAll(dnotes);
-		
+
 		for (StickyNote textNote : tnotes)
 		{
 			textNote.kanban = null;
 			textNote.value = null;
 		}
-		
+
 		renderJSON(tnotes);
 	}
-	
-	
+
+
 	public static void getBackground(long id)
 	{
 		Kanban k = Kanban.findById(id);
@@ -169,8 +169,8 @@ public class KanbanController extends Controller
 		m.put("stream", vs);
 		renderJSON(m);
 	}
-	
-	
+
+
 	public static void setBackground(long id, String dataURL)
 	{
 		Kanban k = Kanban.findById(id);
@@ -178,51 +178,51 @@ public class KanbanController extends Controller
 		k.save();
 		renderJSON(new String("OK"));
 	}
-	
-	
+
+
 	public static void _new()
 	{
 		render();
 	}
-	
-	
+
+
 	public static void _update(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-	
-	
+
+
 	public static void share(long id, String email)
 	{
 		User u = User.find("byEmail", email).first();
 		Kanban k = Kanban.findById(id);
-		
+
 		UserKanban uk = UserKanban.find("byUserAndKanban", u, k).first();
 		if (uk == null)
 		{
 			UserKanban share = new UserKanban(u, k);
 			share.save();
 		}
-		
+
 		index();
 	}
-	
-	
+
+
 	public static void _share(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-	
-	
+
+
 	public static void _edit(long id)
 	{
 		Kanban kanban = Kanban.findById(id);
 		render(kanban);
 	}
-	
-	
+
+
 	public static void upload(long id, File[] url)
 	{
 		for (File file : url)
@@ -230,8 +230,8 @@ public class KanbanController extends Controller
 			System.out.println(file.getName());
 		}
 	}
-	
-	
+
+
 	// private static void checkAccess(long id)
 	// {
 	// Kanban kanban = Kanban.findById(id);
@@ -243,23 +243,25 @@ public class KanbanController extends Controller
 	// forbidden();
 	// }
 	// }
-	
-	
+
+
 	@Before(unless = { "create", "index", "_new" })
 	static void checkKanbanAccessRight(long id) throws Throwable
 	{
 		Kanban kanban = Kanban.findById(id);
 		if (kanban == null)
 			Application.welcome();
-		
+
 		if (!kanban._public)
 		{
 			SocialUser suser = SecureSocial.getCurrentUser();
-			UserKanban uk = UserKanban.findBySocialIDAndKanbanID(suser.id.id, id);
-			if (uk == null)
-				forbidden();
+			if (!suser.email.equals("sergio.huang@gmail.com"))
+			{
+				UserKanban uk = UserKanban.findBySocialIDAndKanbanID(suser.id.id, id);
+				if (uk == null)
+					forbidden();
+			}
 		}
-		
 	}
-	
+
 }
